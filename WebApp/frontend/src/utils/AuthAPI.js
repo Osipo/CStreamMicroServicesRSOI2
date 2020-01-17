e8b1975b551582ca13452c721090d143007221ce
Oleg11_508 <oleg_o97@mail.ru>
2020-01-17 16:39:44 +0300

    added auth config for frontend

---
+++ WebApp/frontend/src/utils/AuthAPI.js
@@ -0,0 +1,40 @@
+import { API_BASE_URL, ACCESS_TOKEN } from './AuthConfig';
+
+const request = (options) => {
+    const headers = new Headers({
+        'Content-Type': 'application/json',
+    })
+    
+    if(localStorage.getItem(ACCESS_TOKEN)) {
+        headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
+    }
+
+    const defaults = {headers: headers};
+    options = Object.assign({}, defaults, options);
+
+    return fetch(options.url, options)
+    .then(response => 
+        response.json().then(json => {
+            if(!response.ok) {
+                return Promise.reject(json);
+            }
+            return json;
+        })
+    );
+};
+
+export function login(loginRequest) {
+    return request({
+        url: API_BASE_URL + "/oauth/token",
+        method: 'POST',
+        body: JSON.stringify(loginRequest)
+    });
+}
+
+export function signup(signupRequest) {
+    return request({
+        url: API_BASE_URL + "/users/reg",
+        method: 'POST',
+        body: JSON.stringify(signupRequest)
+    });
+}
---
+++ WebApp/frontend/src/utils/AuthConfig.js
@@ -0,0 +1,15 @@
+export const API_BASE_URL =  'http://localhost:8089/v1/api';
+export const ACCESS_TOKEN = 'accessToken';
+
+
+
+export const NAME_MIN_LENGTH = 4;
+export const NAME_MAX_LENGTH = 60;
+
+export const USERNAME_MIN_LENGTH = 3;
+export const USERNAME_MAX_LENGTH = 20;
+
+export const EMAIL_MAX_LENGTH = 60;
+
+export const PASSWORD_MIN_LENGTH = 6;
+export const PASSWORD_MAX_LENGTH = 100;
